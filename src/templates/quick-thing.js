ae493dd107a075185ae5a174ffcbbfc21d4837a9
Konstantin <konstantin.kaminskiy@outlook.com>
2025-03-02 09:53:40 -0500

    fixing errors noticed during deployment

--- src/templates/quick-thing.js
+++ src/templates/quick-thing.js
@@ -1,10 +1,46 @@
 import React from "react";
-import { graphql, Link } from "gatsby";
+import { useStaticQuery, graphql, Link } from "gatsby";
 import Layout from "../components/layout";
 
-export default function QuickThingTemplate({ data }) {
-  const { markdownRemark } = data;
-  const { frontmatter, html } = markdownRemark;
+export default function QuickThingTemplate({ id }) {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        nodes {
+          id
+          html
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            slug
+            title
+          }
+        }
+      }
+    }
+  `);
+
+  // Find the specific quick thing by id
+  const quickThing = data.allMarkdownRemark.nodes.find(
+    (node) => node.id === id
+  );
+
+  // If no quick thing is found, return a simple message
+  if (!quickThing) {
+    return (
+      <Layout>
+        <article className="blog-post">
+          <div>Quick thing not found</div>
+          <div className="blog-post-footer">
+            <Link to="/quick-things" className="back-to-home">
+              ← Back to Quick Things
+            </Link>
+          </div>
+        </article>
+      </Layout>
+    );
+  }
+
+  const { frontmatter, html } = quickThing;
 
   return (
     <Layout>
@@ -30,16 +66,3 @@ export default function QuickThingTemplate({ data }) {
     </Layout>
   );
 }
-
-export const pageQuery = graphql`
-  query ($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      html
-      frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        slug
-        title
-      }
-    }
-  }
-`;
--- src/templates/quote.js
+++ src/templates/quote.js
@@ -1,10 +1,45 @@
 import React from "react";
-import { graphql, Link } from "gatsby";
+import { useStaticQuery, graphql, Link } from "gatsby";
 import Layout from "../components/layout";
 
-export default function QuoteTemplate({ data }) {
-  const { markdownRemark } = data;
-  const { frontmatter, html } = markdownRemark;
+export default function QuoteTemplate({ id }) {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        nodes {
+          id
+          html
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            slug
+            author
+            source
+          }
+        }
+      }
+    }
+  `);
+
+  // Find the specific quote by id
+  const quote = data.allMarkdownRemark.nodes.find((node) => node.id === id);
+
+  // If no quote is found, return a simple message
+  if (!quote) {
+    return (
+      <Layout>
+        <article className="blog-post">
+          <div>Quote not found</div>
+          <div className="blog-post-footer">
+            <Link to="/quotes" className="back-to-home">
+              ← Back to Quotes
+            </Link>
+          </div>
+        </article>
+      </Layout>
+    );
+  }
+
+  const { frontmatter, html } = quote;
 
   return (
     <Layout>
@@ -24,17 +59,3 @@ export default function QuoteTemplate({ data }) {
     </Layout>
   );
 }
-
-export const pageQuery = graphql`
-  query ($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      html
-      frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        slug
-        author
-        source
-      }
-    }
-  }
-`;
